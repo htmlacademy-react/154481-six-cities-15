93173ed2f2f82793ebf45188a03b03c7320a7b9f
Maria <afterlifelineonomega@gmail.com>
2024-04-03 20:53:48 +0300

    Добавляет в userSlice информацию о user

--- src/components/types/state.ts
+++ src/components/types/state.ts
@@ -2,6 +2,7 @@ import { store } from '../store';
 import { TCityName } from './types';
 import { TGeneralOffer } from './offers';
 import { AuthorizationStatus } from '../../const';
+import { TUserData } from './usert-data';
 
 export type TInterfaceReducer = {
   activeCity: TCityName;
@@ -14,6 +15,7 @@ export type TDataReducer = {
 
 export type TUserReducer = {
   authorizationStatus: AuthorizationStatus;
+  user: TUserData | null;
 }
 
 export type TState = ReturnType<typeof store.getState>;
