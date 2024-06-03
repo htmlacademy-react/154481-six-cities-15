f7723c6d53421bd77be6ce0a9d0af7e19f96fe9c
Maria <afterlifelineonomega@gmail.com>
2024-06-03 15:01:30 +0300

    Исправляет ошибку импорта

--- src/components/store/middlewares/redirect.ts
+++ src/components/store/middlewares/redirect.ts
@@ -1,7 +1,7 @@
 import {PayloadAction} from '@reduxjs/toolkit';
 import {Middleware} from 'redux';
 import browserHistory from '../../../browser-history';
-import { rootReducer } from '..';
+import { rootReducer } from '../root-reducer';
 
 type Reducer = ReturnType<typeof rootReducer>;
 
